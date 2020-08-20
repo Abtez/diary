d43c26755fd5b45ba94e4e4a9766000ead700ccf
Abzed <ibraabzed@gmail.com>
2020-08-20 14:58:21 +0300

    add style to nav bar using jquery

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+  myName = 'My Diary';
+ 
   ngOnInit(): void {
-  }
+    $(document).ready(function () {
+      $('#darktheme').click(function () {
+        $('body').css('background-color', 'black');
+        $('body').css('color', 'white');
+        $('#darktheme').hide();
+        $('#lightheme').show();
+      });
+      $('#lightheme').click(function () {
+        $('body').css('background-color', 'white');
+        $('body').css('color', 'black');
+        $('#lightheme').hide();
+        $('#darktheme').show();
+      });
+     
+      $("#submit").click(function(){
+        let diaryName = $('#diaryname').val();
+        console.log(diaryName);
+        $("#appname").css({
+         'color':'black',
+         'text-transform':'capitalize',
+         'font-size':'bolder' 
+        });
+        $("#appname").text(diaryName);
 
+      })
+    });
+  }
 }
